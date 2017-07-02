dffddba619890a6781b0c05288b495a8138a3599
jakubferenc <jakubferenc1911@gmail.com>
2017-07-02 14:50:53 +0200

    Remove top comments

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,49 +1,5 @@
 <?php
 
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
-
-
-/**
- * WordPress Multi-Environment Config
- * 
- * Loads config file based on current environment, environment can be set
- * in either the environment variable 'WP_ENV' or can be set based on the 
- * server hostname.
- * 
- * This also overrides the option_home and option_siteurl settings in the 
- * WordPress database to ensure site URLs are correct between environments.
- * 
- * Common environment names are as follows, though you can use what you wish:
- * 
- *   production
- *   staging
- *   development
- * 
- * For each environment a config file must exist named wp-config.{environment}.php
- * with any settings specific to that environment. For example a development 
- * environment would use the config file: wp-config.development.php
- * 
- * Default settings that are common to all environments can exist in wp-config.default.php
- * 
- * @package    Studio 24 WordPress Multi-Environment Config
- * @version    1.0.1
- * @author     Studio 24 Ltd  <info@studio24.net>
- */
-
-
 // Absolute path to the WordPress directory
 if (!defined('ABSPATH')) {
     define('ABSPATH', dirname(__FILE__) . '/');
@@ -59,7 +15,7 @@ define('WP_ENV_PRODUCTION_HOSTNAME', 'novamedia.ff.cuni.cz');
 if (getenv('WP_ENV') !== false) {
     // Filter non-alphabetical characters for security
     define('WP_ENV', preg_replace('/[^a-z]/', '', getenv('WP_ENV')));
-} 
+}
 
 // Define site host
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && !empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
